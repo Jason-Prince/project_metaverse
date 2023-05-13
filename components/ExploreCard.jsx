627ff96b500ac0661fddb6ce5dc7a1168cf67509
jason-prince <jasoncprince@gmail.com>
2023-05-12 21:53:39 -0600

    finished explore and explorecard

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,5 +1,43 @@
 'use client'
 
-const ExploreCard = () => <div>Explore Card</div>
+import { motion } from 'framer-motion'
+import { fadeIn } from '../utils/motion'
+
+const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
+  <motion.div
+    variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
+    className={`relative ${
+      active === id ? 'flex-[10] lg:flex-[3.5]' : 'flex-[2] lg:flex-[0.5]'
+    } flex h-[700px] min-w-[170px] cursor-pointer items-center justify-center transition-[flex] duration-[0.7] ease-out-flex`}
+    onClick={() => handleClick(id)}
+  >
+    <img
+      src={imgUrl}
+      alt={title}
+      className='absolute h-full w-full rounded-[24px] object-cover'
+    />
+    {active !== id ? (
+      <h3 className='absolute z-0 text-[18px] font-semibold text-white sm:text-[26px] lg:bottom-20 lg:origin-[0,0] lg:rotate-[-90deg]'>
+        {title}
+      </h3>
+    ) : (
+      <div className='absolute bottom-0 w-full flex-col justify-start rounded-b-[24px] bg-[rgba(0,0,0,0.5)] p-8'>
+        <div className='glassmorphism mb-[16px] flex h-[60px] w-[60px] items-center justify-center rounded-[24px]'>
+          <img
+            src='/headset.svg'
+            alt='headset'
+            className='h-1/2 w-1/2 object-contain'
+          />
+        </div>
+        <p className='text-[16px] font-normal uppercase leading-[20px] text-white'>
+          Enter the Metaverse
+        </p>
+        <h2 className='mt-[24px] text-[24px] font-semibold text-white sm:text-[32px]'>
+          {title}
+        </h2>
+      </div>
+    )}
+  </motion.div>
+)
 
 export default ExploreCard
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,5 +1,42 @@
 'use client'
 
-const Explore = () => <section>Explore section</section>
+import { motion } from 'framer-motion'
+import { staggerContainer } from '../utils/motion'
+import { ExploreCard, TitleText, TypingText } from '../components'
+import { exploreWorlds } from '../constants'
+import { useState } from 'react'
+
+const Explore = () => {
+  const [active, setActive] = useState('world-2')
+
+  return (
+    <section className='xs:p-8 px-6 py-12 sm:p-16' id='explore'>
+      <motion.div
+        variants={staggerContainer}
+        initial='hidden'
+        whileInView='show'
+        viewport={{ once: false, amount: 0.25 }}
+        className='mx-auto flex w-full flex-col 2xl:max-w-[1280px]'
+      >
+        <TypingText title='| The World' textStyles='text-center' />
+        <TitleText
+          title='Choose the world you want to explore'
+          textStyles='text-center'
+        />
+        <div className='mt-[50px] flex min-h-[70vh] flex-col gap-5 lg:flex-row'>
+          {exploreWorlds.map((world, index) => (
+            <ExploreCard
+              key={world.id}
+              {...world}
+              index={index}
+              active={active}
+              handleClick={setActive}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </section>
+  )
+}
 
 export default Explore
