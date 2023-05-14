8ae207bf5ae93cac1fcaa1f84b64c0a76c8e6e32
jason-prince <jasoncprince@gmail.com>
2023-05-13 23:42:33 -0600

    finished get started

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -1,3 +1,12 @@
-const StartSteps = () => <div>start steps</div>
+const StartSteps = ({ number, text }) => (
+  <div className='flex flex-row items-center justify-center'>
+    <div className='flex h-[70px] w-[70px] items-center justify-center rounded-[24px] bg-[#323f5d]'>
+      <p className='text-[20px] font-bold text-white'>0{number}</p>
+    </div>
+    <p className='ml-[30px] flex-1 text-[18px] font-normal leading-[32px] text-[#b0b0b0]'>
+      {text}
+    </p>
+  </div>
+)
 
 export default StartSteps
