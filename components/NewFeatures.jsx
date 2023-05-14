db773d4dec8f2fecf4aeadf27bd72d0093c7660d
jason-prince <jasoncprince@gmail.com>
2023-05-14 00:16:06 -0600

    finished whats new

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,3 +1,15 @@
-const NewFeatures = () => <div>New Features</div>
+const NewFeatures = ({ imgUrl, title, subtitle }) => (
+  <div className='flex min-w-[210px] flex-1 flex-col sm:max-w-[250px]'>
+    <div className='flex h-[70px] w-[70px] items-center justify-center rounded-[24px] bg-[#323f5d]'>
+      <img src={imgUrl} alt='icon' className='h-1/2 w-1/2 object-contain' />
+    </div>
+    <h1 className='mt-[26px] text-[24px] font-bold leading-[30px] text-white'>
+      Title {title}
+    </h1>
+    <p className='mt-[16px] flex-1 text-[18px] font-normal leading-[32px] text-[#b0b0b0]'>
+      {subtitle}
+    </p>
+  </div>
+)
 
 export default NewFeatures
--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,5 +1,43 @@
 'use client'
 
-const WhatsNew = () => <section>What's new section</section>
+import { motion } from 'framer-motion'
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion'
+import { NewFeatures, TitleText, TypingText } from '../components'
+import { newFeatures } from '../constants'
+
+const WhatsNew = () => (
+  <section className='xs:p-8 relative z-10 px-6 py-12 sm:p-16'>
+    <motion.div
+      variants={staggerContainer}
+      initial='hidden'
+      whileInView='show'
+      viewport={{ once: 'false', amount: 0.25 }}
+      className='mx-auto flex w-full flex-col gap-8 lg:flex-row 2xl:max-w-[1280px]'
+    >
+      <motion.div
+        variants={fadeIn('right', 'tween', 0.2, 1)}
+        className='flex flex-[0.75] flex-col justify-center'
+      >
+        <TypingText title="| What's new?" />
+        <TitleText title={<>What's new about Metaversus?</>} />
+        <div className='mt-[48px] flex flex-wrap justify-between gap-[24px] '>
+          {newFeatures.map(feature => (
+            <NewFeatures key={feature.title} {...feature} />
+          ))}
+        </div>
+      </motion.div>
+      <motion.div
+        variants={planetVariants('right')}
+        className='flex flex-1 items-center justify-center'
+      >
+        <img
+          src='/whats-new.png'
+          alt='get-started'
+          className='h-[90%] w-[90%] object-contain'
+        />
+      </motion.div>
+    </motion.div>
+  </section>
+)
 
 export default WhatsNew
